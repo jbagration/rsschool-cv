0d7ad056894e1bb161528503d8b9429abaf10ef2
jbagration <blissdery@gmail.com>
2022-12-21 16:38:32 +0500

    modified:   assets/css/style.css 	modified:   assets/js/main.js 	modified:   index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,26 +26,6 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*swiper projects
-let swiperProjects = new Swiper(".projects__container", {
-    loop: true,
-    spaceBetween: 24,
-  
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-    breakpoints: {
-        1200: {
-          slidesPerView: 2,
-          spaceBetween: -20,
-        },
-    },
-});
-
 /*email js*/
 const contactForm = document.getElementById('contact-form'),
       contactName = document.getElementById('contact-name'),
